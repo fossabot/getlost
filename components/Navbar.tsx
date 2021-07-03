5fb6c2d97b8725012f144e8bd16118edf54828ca
Ahmad Ainul.R <ahmad@ainul.tech>
2021-07-03 17:06:18 +0700

    Finish little home page

---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { Link, Row, Text } from '@geist-ui/react';
+
+export default function Navbar() {
+  return (
+    <Row className='border-b border-gray-200 !fixed !bg-gray-50 z-10'>
+      <div className='text-gray-700 flex justify-around items-center !min-w-[100vw]'>
+        <div className='flex items-center justify-between'>
+          <img src='/logo.svg' alt='GetLost' width='25px' />
+          <Text b p className='ml-2 text-2xl'>
+            GetLost
+          </Text>
+        </div>
+        <div>
+          <Link block>Sign Up</Link>
+          <Link block>Sign In</Link>
+        </div>
+      </div>
+    </Row>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,109 @@
-import { Page, Text } from "@geist-ui/react";
-import Head from "next/head";
+import { Button, Divider, Input, Page, Text } from '@geist-ui/react';
+import Head from 'next/head';
+import Navbar from '../components/Navbar';
+import FeatureCard, { NavBarProps } from '../components/FeatureCard';
+import { Code, Lock, Zap } from '@geist-ui/react-icons';
+import Shield from '../components/Shield';
+
+const features: NavBarProps[] = [
+  {
+    feature:
+      'Password protect website with only a single line of code which GetLost provides',
+    children: <Code />,
+  },
+  {
+    feature:
+      'Users log in to your application through a beautiful responsive page hosted on GetLost',
+    children: <Shield />,
+  },
+  {
+    feature:
+      'GetLost is framework agnostic. It works with the framework of your choice',
+    children: <Zap />,
+  },
+  {
+    feature:
+      'GetLost hashes and encrypts data at multiple levels so that your data stays highly secure',
+    children: <Lock />,
+  },
+];
 
 export default function Home() {
   return (
-    <Page>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Text h1>StaticShield</Text>
-    </Page>
+    <div className='text-center text-gray-900 bg-gray-50'>
+      <div className='block'>
+        <Navbar />
+      </div>
+      <Page>
+        <Head>
+          <title>GetLost</title>
+          <link rel='icon' href='/logo.svg' />
+        </Head>
+        <img src='/logo.svg' alt='' className='w-40 h-40 mx-auto mt-16' />
+        <Text
+          className='!font-extrabold mt-12 text-5xl sm:text-7xl bg-gradient-to-r from-black via-gray-600 to-gray-500 !text-transparent bg-clip-text'
+          h1>
+          Get{''}
+          <span className='px-2 bg-gray-900 rounded-xl text-gray-50'>
+            Lost
+          </span>
+        </Text>
+        <Text className='text-2xl'>
+          GetLost is the easiest way to{' '}
+          <span className='z-10 mx-1 heading-underline'>password protect</span>{' '}
+          <br />
+          your static and dyanmic sites.
+        </Text>
+
+        <div className='mt-12'>
+          <Input
+            size='large'
+            width='240px'
+            status='success'
+            placeholder='Enter your email…'
+          />
+          <Button
+            auto
+            type='success'
+            className='!inline-block ml-2 !shadow-md mt-5'>
+            Get started →
+          </Button>
+        </div>
+
+        <Divider className='!mt-20 !mb-12'>
+          <span className='-mx-6 text-gray-500 bg-gray-50'>FEATURES</span>
+        </Divider>
+
+        <div className='flex flex-wrap'>
+          {features.map((feature) => {
+            return (
+              <FeatureCard
+                key={feature.feature}
+                feature={feature.feature}
+                children={feature.children}
+              />
+            );
+          })}
+        </div>
+      </Page>
+      <style jsx>{`
+        span.heading-underline {
+          position: relative;
+        }
+        span.heading-underline::after {
+          content: '';
+          background-color: #bfdbfe;
+          border-radius: 3px;
+          transform: rotate(-2deg);
+          margin: auto;
+          position: absolute;
+          bottom: -0.125rem;
+          left: -0.5rem;
+          right: -0.5rem;
+          height: 2rem;
+          z-index: -1;
+        }
+      `}</style>
+    </div>
   );
 }
